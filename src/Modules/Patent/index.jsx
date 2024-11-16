8686b2d19e68358a9ff358e2368a0e3e182b6073
arshsinha22 <arshsinha22@gmail.com>
2024-11-17 03:11:37 +0530

    Removed unnecessary scripts form index.jsx

--- src/Modules/Patent/index.jsx
+++ src/Modules/Patent/index.jsx
@@ -19,8 +19,6 @@ import RequestModifications from "./components/Director/RequestModifications";
 import ApplicationView from "./components/Applicant/ApplicationView";
 import DownloadsPage from "./components/PCCAdmin/DownloadsPage";
 import InsightsPage from "./components/PCCAdmin/InsightsPage";
-<script src="https://cdn.canvasjs.com/canvasjs.min.js"></script>
-
 
 function PatentPage() {
   const [activeTab, setActiveTab] = useState("0");
@@ -55,8 +53,6 @@ function PatentPage() {
       behavior: "smooth",
     });
   };
-
-
 
   return (
     <>
